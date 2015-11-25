3195bc0a852e00640d79414a384f57cc8c4baa95
Niklas Blomqvist <ntjblomqvist@gmail.com>
2015-11-25 10:26:58 +0100

    Wall bug fixes and door bug.

--- Scripts/OpenDoor.cs
+++ Scripts/OpenDoor.cs
@@ -16,19 +16,19 @@ public class OpenDoor : MonoBehaviour {
 
     public void openDoor()
     {
-        if(isOpen == false && !GetComponent<Animation>().IsPlaying("door_animation"))
+        if(isOpen == false && !GetComponent<Animation>().IsPlaying("door_animation_1"))
         {
             // Sets time to start animation, what speed and then plays it
-            GetComponent<Animation>()["door_animation"].time = 0.0f;
-            GetComponent<Animation>()["door_animation"].speed = 1.0f;
+            GetComponent<Animation>()["door_animation_1"].time = 0.0f;
+            GetComponent<Animation>()["door_animation_1"].speed = 1.0f;
             GetComponent<Animation>().Play();
             GetComponent<AudioSource>().Play();
             isOpen = true;
         }
-        else if (isOpen == true && !GetComponent<Animation>().IsPlaying("door_animation"))
+        else if (isOpen == true && !GetComponent<Animation>().IsPlaying("door_animation_1"))
         {
-            GetComponent<Animation>()["door_animation"].time = GetComponent<Animation>()["door_animation"].length;
-            GetComponent<Animation>()["door_animation"].speed = -1.0f;
+            GetComponent<Animation>()["door_animation_1"].time = GetComponent<Animation>()["door_animation_1"].length;
+            GetComponent<Animation>()["door_animation_1"].speed = -1.0f;
             GetComponent<Animation>().Play();
             isOpen = false;
         }
