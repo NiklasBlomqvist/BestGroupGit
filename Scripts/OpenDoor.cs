64eb7342cd067658e9a616e0be07c1d58e763da5
Niklas Blomqvist <ntjblomqvist@gmail.com>
2015-11-26 19:48:10 +0100

    Added prefab IndoorDoor with animation

--- Scripts/OpenDoor.cs
+++ Scripts/OpenDoor.cs
@@ -16,19 +16,19 @@ public class OpenDoor : MonoBehaviour {
 
     public void openDoor()
     {
-        if(isOpen == false && !GetComponent<Animation>().IsPlaying("door_animation_1"))
+        if(isOpen == false && !GetComponent<Animation>().IsPlaying("OpenDoor"))
         {
             // Sets time to start animation, what speed and then plays it
-            GetComponent<Animation>()["door_animation_1"].time = 0.0f;
-            GetComponent<Animation>()["door_animation_1"].speed = 1.0f;
+            GetComponent<Animation>()["OpenDoor"].time = 0.0f;
+            GetComponent<Animation>()["OpenDoor"].speed = 1.0f;
             GetComponent<Animation>().Play();
-            GetComponent<AudioSource>().Play();
+            //GetComponent<AudioSource>().Play();
             isOpen = true;
         }
-        else if (isOpen == true && !GetComponent<Animation>().IsPlaying("door_animation_1"))
+        else if (isOpen == true && !GetComponent<Animation>().IsPlaying("OpenDoor"))
         {
-            GetComponent<Animation>()["door_animation_1"].time = GetComponent<Animation>()["door_animation_1"].length;
-            GetComponent<Animation>()["door_animation_1"].speed = -1.0f;
+            GetComponent<Animation>()["OpenDoor"].time = GetComponent<Animation>()["OpenDoor"].length;
+            GetComponent<Animation>()["OpenDoor"].speed = -1.0f;
             GetComponent<Animation>().Play();
             isOpen = false;
         }
